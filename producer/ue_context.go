3d5edea15907006040350dc6e63e261df97936f6
AshithaCDAC <ashitha@cdac.in>
2024-04-09 12:02:34 +0530

    commit005

--- producer/ue_context.go
+++ producer/ue_context.go
@@ -600,7 +600,7 @@ func HandleRegistrationStatusUpdateRequest(request *httpwrapper.Request) *httpwr
 	//logger.CommLog.Infof("---problemdetailstatus: %d", int(msg.ProblemDetails.(*models.ProblemDetails).Status))
 	//logger.CommLog.Infof("---problemdetails: %v", msg.ProblemDetails.(*models.ProblemDetails))
 	if msg.ProblemDetails != nil {
-		logger.CommLog.Info("---testing---")
+		logger.CommLog.Info("---testing problemdetails nil---")
 		logger.CommLog.Info("---problemdetailstatus:", int(msg.ProblemDetails.(*models.ProblemDetails).Status))
 		logger.CommLog.Info("---problemdetails:", msg.ProblemDetails.(*models.ProblemDetails))
 		return httpwrapper.NewResponse(int(msg.ProblemDetails.(*models.ProblemDetails).Status), nil, msg.ProblemDetails.(*models.ProblemDetails))
